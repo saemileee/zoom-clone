4d9cd24718959dfa39c433b71fb2729aa93f223c
Saem <saemilee01@gmail.com>
2023-05-26 02:00:59 +0900

    Feat: socket에 프로퍼티를 추가하여 닉네임 설정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,16 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+function decodeMessage(json) {
+  return JSON.parse(json);
+}
 
 function handleOpen() {
   console.log('Connected to Server ✅');
@@ -8,22 +18,30 @@ function handleOpen() {
 
 socket.addEventListener('open', handleOpen);
 
+//3. 백단에서 받은 메세지를 list로 렌더링
 socket.addEventListener('message', (message) => {
-  console.log('New message: ', message.data);
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener('close', () => {
   console.log('Disconnected from Server❌');
 });
 
-setTimeout(() => {
-  //프론트에서 백엔드로 보냄
-  socket.send('hello from the browser!');
-}, 4000);
-
 function handleSubmit(e) {
   e.preventDefault();
   const input = messageForm.querySelector('input');
-  socket.send(input.value);
+
+  //1. 프론트에서 백엔드로 보냄
+  socket.send(makeMessage('new_message', input.value));
 }
+
+function handleNickSubmit(e) {
+  e.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+}
+
 messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import WebSocket from 'ws';
 import express from 'express';
+import { parse } from 'path';
 
 const app = express();
 
@@ -20,15 +21,27 @@ function onSocketClose() {
 }
 
 const sockets = [];
-const msgs = [];
 
 wss.on('connection', (socket) => {
   sockets.push(socket);
+  //닉네임을 입력하지 않으면 익명으로
+  socket['nickname'] = 'Anonymous';
   console.log('Connected to Browser ✅');
   socket.on('close', onSocketClose);
-  socket.on('message', (message) => {
-    //프론트에서 받은 메세지를 배열로 저장해 다시 프론트로 보냄 (다른 브라우저에서 보내도 같은 wws서버로 보내면 확인할 수 있음)
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case 'new_message':
+        //2. 프론트에서 받은 메세지를 배열로 저장해 다시 프론트로 보냄 (다른 브라우저에서 보내도 같은 wws서버로 보내면 확인할 수 있음)
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}:${message.payload}`)
+        );
+        break;
+      case 'nickname':
+        //소켓은 객체라 새로운 key와 value값을 줌
+        socket['nickname'] = message.payload;
+        break;
+    }
   });
   console.log(sockets);
 });
