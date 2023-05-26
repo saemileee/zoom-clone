de498e0704a744be48e555ae73b6a9f894cb9afd
Saem <saemilee01@gmail.com>
2023-05-26 15:09:38 +0900

    Feat: socket.io로 프론트, 백엔드 통신 (socket.emit으로 메세지와 콜백함수 보내기)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,51 +1,18 @@
-const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('#message');
-const nickForm = document.querySelector('#nick');
-const socket = new WebSocket(`ws://${window.location.host}`);
+//io는 자동적으로 백엔드의 socket.io와 연결해 주는 function
+const socket = io();
+const welcome = document.getElementById('welcome');
+const form = welcome.querySelector('form');
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-}
-
-function decodeMessage(json) {
-  return JSON.parse(json);
-}
-
-function handleOpen() {
-  console.log('Connected to Server ✅');
-}
-
-socket.addEventListener('open', handleOpen);
-
-//3. 백단에서 받은 메세지를 list로 렌더링
-socket.addEventListener('message', (message) => {
-  const li = document.createElement('li');
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-socket.addEventListener('close', () => {
-  console.log('Disconnected from Server❌');
-});
-
-function handleSubmit(e) {
+function handleRoomSubmit(e) {
   e.preventDefault();
-  const input = messageForm.querySelector('input');
-  //1. 프론트에서 백엔드로 보냄
-  socket.send(makeMessage('new_message', input.value));
+  const input = form.querySelector('input');
+  //1. 프론트에서 입력받은 input.value를 io로 백엔드로 보냄
+  //socket.emit 첫번째 인자 : event 이름, 두번째 인자: payload, 세번째 인자: 서버에서 호출하는 Function
+  socket.emit('enter_room', { payload: input.value }, () => {
+    console.log('server is done');
+  });
 
-  //4. 내가 보내는 메세지는 굳이 서버에 전송하지 않고 렌더링
-  const li = document.createElement('li');
-  li.innerText = `You: ${input.value}`;
-  messageList.append(li);
+  input.value = '';
 }
 
-function handleNickSubmit(e) {
-  e.preventDefault();
-  const input = nickForm.querySelector('input');
-  socket.send(makeMessage('nickname', input.value));
-}
-
-messageForm.addEventListener('submit', handleSubmit);
-nickForm.addEventListener('submit', handleNickSubmit);
+form.addEventListener('submit', handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import http from 'http';
-import WebSocket from 'ws';
+import { Server } from 'socket.io';
+// import WebSocket from 'ws';
 import express from 'express';
-import { parse } from 'path';
 
 const app = express();
+const server = http.createServer(app);
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -11,39 +12,51 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
-const handleListen = () => console.log('Hello');
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-function onSocketClose() {
-  console.log('Disconnected from the Browser❌');
-}
-
-const sockets = [];
-
-wss.on('connection', (socket) => {
-  sockets.push(socket);
-  //닉네임을 입력하지 않으면 익명으로
-  socket['nickname'] = 'Anonymous';
-  console.log('Connected to Browser ✅');
-  socket.on('close', onSocketClose);
-  socket.on('message', (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case 'new_message':
-        //2. 프론트에서 받은 메세지를 배열로 저장해 다시 프론트로 보냄 (다른 브라우저에서 보내도 같은 wws서버로 보내면 확인할 수 있음)
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}:${message.payload}`)
-        );
-        break;
-      case 'nickname':
-        //소켓은 객체라 새로운 key와 value값을 줌
-        socket['nickname'] = message.payload;
-        break;
-    }
+wsServer.on('connection', (socket) => {
+  //2. 프론트'enter_room'로 부터 받은 메세지를 백엔드 콘솔에 출력함
+  //콜백함수 인자로 프론트에서 받은 2, 3번째 인자를 사용 할 수 있음
+  socket.on('enter_room', (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 5000);
   });
-  console.log(sockets);
 });
 
-server.listen(3000, handleListen);
+// const wss = new WebSocket.Server({ server });
+
+// function onSocketClose() {
+//   console.log('Disconnected from the Browser❌');
+// }
+
+// const sockets = [];
+
+// wss.on('connection', (socket) => {
+//   sockets.push(socket);
+//   //닉네임을 입력하지 않으면 익명으로
+//   socket['nickname'] = 'Anonymous';
+//   console.log('Connected to Browser ✅');
+//   socket.on('close', onSocketClose);
+//   socket.on('message', (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case 'new_message':
+//         //2. 프론트에서 받은 메세지를 배열로 저장해 다시 프론트로 보냄 (다른 브라우저에서 보내도 같은 wws서버로 보내면 확인할 수 있음)
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}:${message.payload}`)
+//         );
+//         break;
+//       case 'nickname':
+//         //소켓은 객체라 새로운 key와 value값을 줌
+//         socket['nickname'] = message.payload;
+//         break;
+//     }
+//   });
+//   console.log(sockets);
+// });
+
+const handleListen = () => console.log('Hello');
+httpServer.listen(3000, handleListen);
